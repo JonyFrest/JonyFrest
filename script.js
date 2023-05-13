e45659ee89c867dd12ca2539240eadfd4a67bd89
Zafabek Yakubov <jonyfrestuz@gmail.com>
2023-05-13 18:08:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var person = null;
+if(confirm("Welcome to my website Portfolio!")) {
+    person = prompt("Please, enter your name:")
+    alert("Hello, " + person)
+} else {
+    alert("You clicked the «Cancel» button. Click on «OK» to complate the action!")
+}
